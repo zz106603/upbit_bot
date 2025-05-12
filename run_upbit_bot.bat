54bda0f0eaad6f4cd7e3f770a1eca54870f42fe6
정윤환 <629jyh7@naver.com>
2025-05-12 16:41:00 +0900

    feat: bat 파일 경로 수정

--- run_upbit_bot.bat
+++ run_upbit_bot.bat
@@ -1,9 +1,7 @@
 @echo off
 
-REM Conda activate + 환경명으로 실행
 CALL conda activate C:\conda-envs\upbit-bot
 
-REM 현재 디렉토리 기준으로 실행 (상대 경로)
-python upbit_alert_bot.py
+C:\conda-envs\upbit-bot\python.exe upbit_alert_bot.py
 
 pause
